0785ad1effe593076ba9b9550b51feaae87cca4d
s1140416-ux <s1140416@dmhs.kh.edu.tw>
2026-05-22 10:54:35 +0800

    Add files via upload

---
+++ 407 05.cpp
@@ -0,0 +1,13 @@
+#include <io stream>
+using namespace std;
+
+int main () {
+	cout << "1.㎒솔촒쾧" << end1;
+	cout << "2.㎒솔   촒쾧" << end1;
+	cout << "3.㎒솔/t촒쾧" << end1;
+	cout << "4.㎒솔/n촒쾧" << end1;
+	cout << "\"5.㎒솔촒쾧\"" << end1;
+	cout << "\'6.㎒솔촒쾧\'" << end1;
+	cout << "\\7.㎒솔촒쾧"\\ << end1;
+	reture 0; 
+}
