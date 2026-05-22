ec5f1044d11ba02faec61a125a5022f85a9ea454
s1140416-ux <s1140416@dmhs.kh.edu.tw>
2026-05-22 11:25:43 +0800

    Add files via upload

--- 407 05.cpp
+++ 407 05.cpp
@@ -1,13 +1,21 @@
-#include <io stream>
+#include <iostream>
 using namespace std;
 
-int main () {
-	cout << "1.㎒솔촒쾧" << end1;
-	cout << "2.㎒솔   촒쾧" << end1;
-	cout << "3.㎒솔/t촒쾧" << end1;
-	cout << "4.㎒솔/n촒쾧" << end1;
-	cout << "\"5.㎒솔촒쾧\"" << end1;
-	cout << "\'6.㎒솔촒쾧\'" << end1;
-	cout << "\\7.㎒솔촒쾧"\\ << end1;
-	reture 0; 
+int main() {
+    int x1 = 5 + 3;    
+    int x2 = 5 - 3;    
+    int x3 = 5 * 3;    
+    int x4 = 5 / 3;    
+    int x5 = 5 % 3;    
+    float x6 = 5 / 3.0;    
+
+
+    cout << "x1 (5+3)  = " << x1 << endl;
+    cout << "x2 (5-3)  = " << x2 << endl;
+    cout << "x3 (5*3)  = " << x3 << endl;
+    cout << "x4 (5/3)  = " << x4 << endl;
+    cout << "x5 (5%3)  = " << x5 << endl;
+    cout << "x6 (5/3.0)= " << x6 << endl;
+
+    return 0;
 }
